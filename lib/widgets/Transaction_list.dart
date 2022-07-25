480fded8329e8eae262fa05717eb6c7041eee8ca
kaibalya jena <kaibalya18j@gmail.com>
2022-07-25 16:19:15 +0530

    app segragation

---
+++ lib/widgets/Transaction_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Transaction_list extends StatefulWidget {
+  @override
+  State<Transaction_list> createState() => _Transaction_listState();
+}
+
+class _Transaction_listState extends State<Transaction_list> {
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
