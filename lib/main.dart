73eaa20e3656e083eccdb3c3f58a860a1922af31
kaibalya jena <kaibalya18j@gmail.com>
2022-07-25 16:33:57 +0530

    transaction list widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
-import 'package:financulator/models/transaction.dart';
+import 'package:financulator/widgets/Transaction_list.dart';
+import 'package:financulator/widgets/Transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(id: "1", title: "raquet", amount: 2800, date: DateTime.now()),
-    Transaction(id: "2", title: "injection", amount: 450, date: DateTime.now()),
-    Transaction(id: "3", title: "strap", amount: 1050, date: DateTime.now()),
-    Transaction(id: "4", title: "specs", amount: 2200, date: DateTime.now()),
-  ];
-
   // String titleinp = "";
   // String amountinp = "";
   final title_controller = TextEditingController();
@@ -71,56 +64,7 @@ class MyApp extends StatelessWidget {
                             onPressed: () {}, child: Text("add transaction"))
                       ])),
             ),
-            Column(
-              children: [
-                ...transactions.map((tx) {
-                  return Card(
-                    color: Color(0xFFD9F8C4),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    elevation: 10,
-                    child: Row(children: [
-                      Container(
-                        margin: EdgeInsets.only(
-                            right: 30, left: 15, top: 15, bottom: 15),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Color(0xFF809A6F), width: 4),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Text(
-                          '₹${tx.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF3CCF4E),
-                              fontSize: 20),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(bottom: 6),
-                            child: Text(
-                              tx.title,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 17),
-                            ),
-                          ),
-                          Container(
-                            child: Text(
-                              DateFormat().add_yMMMd().format(tx.date),
-                              style: TextStyle(color: Color(0xFF2B7A0B)),
-                            ),
-                          )
-                        ],
-                      )
-                    ]),
-                  );
-                }).toList()
-              ],
-            )
+            TransactionList()
           ],
         ),
       ),
--- lib/widgets/Transaction_list.dart
+++ lib/widgets/Transaction_list.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import '../models/Transaction.dart';
+import 'package:intl/intl.dart';
 
-class Transaction_list extends StatefulWidget {
+class TransactionList extends StatefulWidget {
   @override
-  State<Transaction_list> createState() => _Transaction_listState();
+  State<TransactionList> createState() => _Transaction_listState();
 }
 
-class _Transaction_listState extends State<Transaction_list> {
+class _Transaction_listState extends State<TransactionList> {
+  final List<Transaction> transactions = [
+    Transaction(id: "1", title: "raquet", amount: 2800, date: DateTime.now()),
+    Transaction(id: "2", title: "injection", amount: 450, date: DateTime.now()),
+    Transaction(id: "3", title: "strap", amount: 1050, date: DateTime.now()),
+    Transaction(id: "4", title: "specs", amount: 2200, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Column(
+      children: [
+        ...transactions.map((tx) {
+          return Card(
+            color: Color(0xFFD9F8C4),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 10,
+            child: Row(children: [
+              Container(
+                margin:
+                    EdgeInsets.only(right: 30, left: 15, top: 15, bottom: 15),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFF809A6F), width: 4),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Text(
+                  '₹${tx.amount}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3CCF4E),
+                      fontSize: 20),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      DateFormat().add_yMMMd().format(tx.date),
+                      style: TextStyle(color: Color(0xFF2B7A0B)),
+                    ),
+                  )
+                ],
+              )
+            ]),
+          );
+        }).toList()
+      ],
+    );
   }
 }
