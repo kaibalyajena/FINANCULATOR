8a41c6955b834d901e973b52513b2da350a5872e
kaibalya jena <kaibalya18j@gmail.com>
2022-07-28 14:56:06 +0530

    added on submit

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewTransaction extends StatelessWidget {
   final title_controller = TextEditingController();
   final amount_controller = TextEditingController();
   final Function add_trans_func;
   NewTransaction(this.add_trans_func);
+
+  void submit_trans() {
+    final title_text = title_controller.text;
+    final amount_text = int.parse(amount_controller.text);
+    if (title_text.isEmpty || amount_text <= 0) {
+      return;
+    }
+    add_trans_func(title_controller.text, int.parse(amount_controller.text));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,6 +26,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "title"),
               controller: title_controller,
+              onSubmitted: (_) => {submit_trans()},
               // onChanged: (x) {
               //   titleinp = x;
               // },
@@ -22,16 +34,13 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "amount"),
               controller: amount_controller,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => {submit_trans()},
               // onChanged: (y) {
               //   amountinp = y;
               // },
             ),
-            FlatButton(
-                onPressed: () {
-                  add_trans_func(
-                      title_controller.text, int.parse(amount_controller.text));
-                },
-                child: Text("add transaction"))
+            FlatButton(onPressed: submit_trans, child: Text("add transaction"))
           ])),
     );
   }
