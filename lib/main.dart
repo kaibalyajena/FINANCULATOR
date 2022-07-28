43063375e689afb44da11e92dfe988025f0222cc
kaibalya jena <kaibalya18j@gmail.com>
2022-07-28 15:09:11 +0530

    wrapped with single child scroll

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   // String titleinp = "";
   // String amountinp = "";
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,26 +17,44 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("FINANCULATOR"),
           backgroundColor: Color(0xFF76BA99),
+          actions: [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ))
+          ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Card(
-              color: Color(0xFF76BA99),
-              child: Container(
-                width: double.infinity,
-                child: Center(
-                  child: Text(
-                    "chart here",
-                    style: TextStyle(fontSize: 30),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Card(
+                color: Color(0xFF76BA99),
+                child: Container(
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "chart here",
+                      style: TextStyle(fontSize: 30),
+                    ),
                   ),
                 ),
+                elevation: 7,
+                margin:
+                    EdgeInsets.only(bottom: 30, top: 20, left: 10, right: 10),
               ),
-              elevation: 7,
-              margin: EdgeInsets.only(bottom: 30, top: 20, left: 10, right: 10),
-            ),
-            UserTransactions()
-          ],
+              UserTransactions()
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          foregroundColor: Color(0xFF76BA99),
+          child: Icon(Icons.add),
+          onPressed: null,
         ),
       ),
     );
