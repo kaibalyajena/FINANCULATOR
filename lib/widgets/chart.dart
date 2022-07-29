ca7c30b0af3a4f0a0f6367ac8aab326150887d2b
kaibalya jena <kaibalya18j@gmail.com>
2022-07-29 15:48:59 +0530

    made chart widget

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:financulator/models/Transaction.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recent_trans;
+  Chart(this.recent_trans);
+
+  List<Map<String, Object>> get daily_trans {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      int total_sum = 0;
+      for (var i = 0; i < recent_trans.length; i++) {
+        if (recent_trans[i].date.day == weekDay.day &&
+            recent_trans[i].date.month == weekDay.month &&
+            recent_trans[i].date.year == weekDay.year) {
+          total_sum += recent_trans[i].amount;
+        }
+      }
+      return {"day": DateFormat.E(weekDay), 'amount': total_sum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
